1ccb7e9cb407ea471589a224542d0dc0e51a482d
alynna <arpajaron@gmail.com>
2014-10-20 19:22:25 +0800

    FormInventory

---
+++ KoalaShopPOS/FormInventory.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace KoalaShopPOS
+{
+    partial class FormInventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.tileControl1 = new DevExpress.XtraEditors.TileControl();
+            this.applicationMenu1 = new DevExpress.XtraBars.Ribbon.ApplicationMenu(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.applicationMenu1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Size = new System.Drawing.Size(972, 142);
+            this.pictureEdit1.TabIndex = 0;
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.tileControl1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(969, 542);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "groupControl1";
+            // 
+            // tileControl1
+            // 
+            this.tileControl1.Location = new System.Drawing.Point(90, 176);
+            this.tileControl1.Name = "tileControl1";
+            this.tileControl1.Size = new System.Drawing.Size(164, 115);
+            this.tileControl1.TabIndex = 0;
+            this.tileControl1.Text = "tileControl1";
+            // 
+            // applicationMenu1
+            // 
+            this.applicationMenu1.Name = "applicationMenu1";
+            // 
+            // FormInventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 542);
+            this.Controls.Add(this.pictureEdit1);
+            this.Controls.Add(this.groupControl1);
+            this.Name = "FormInventory";
+            this.Text = "FormInventory";
+            this.Load += new System.EventHandler(this.FormInventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.applicationMenu1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.TileControl tileControl1;
+        private DevExpress.XtraBars.Ribbon.ApplicationMenu applicationMenu1;
+
+
+    }
+}
---
+++ KoalaShopPOS/FormInventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace KoalaShopPOS
+{
+    public partial class FormInventory : DevExpress.XtraEditors.XtraForm
+    {
+        public FormInventory()
+        {
+            InitializeComponent();
+        }
+
+        private void FormInventory_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
