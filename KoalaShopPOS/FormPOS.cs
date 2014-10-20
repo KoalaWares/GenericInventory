330fb74e757d7a3fbee3709c762d819d413ef505
reykobai <reykobai@gmail.com>
2014-10-20 23:02:05 +0800

    hi support,
    last na lang jd ka dha...
    
    nasuko na ko,
    
    rey

---
+++ KoalaShopPOS/FormPOS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace POS_System
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupControl4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textEdit8_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupControl5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
