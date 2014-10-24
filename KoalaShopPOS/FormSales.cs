5e17b387388769c8046d242f50cdd551a50c26e9
alynna <arpajaron@gmail.com>
2014-10-24 21:05:56 +0800

    Hi Koala,
    Made some forms.
    Please verify.
    Thank you.
    
    
    Kind regards,
    Koalingling

---
+++ KoalaShopPOS/FormSales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace KoalaShopPOS
+{
+    public partial class FormSales : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FormSales()
+        {
+            InitializeComponent();
+        }
+    }
+}
