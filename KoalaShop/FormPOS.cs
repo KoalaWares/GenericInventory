cfad72e40eed72fef19714411caa9081187bcbe5
reykobai <reykobai@gmail.com>
2014-11-06 11:15:24 +0800

    Hi Koalas,
    Copied forms..
    
    lelz..

---
+++ KoalaShop/FormPOS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace KoalaShop
+{
+    public partial class FormPOS : DevExpress.XtraEditors.XtraForm
+    {
+        public FormPOS()
+        {
+            InitializeComponent();
+        }
+
+        private void groupControl4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textEdit8_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupControl5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnChangePass_Click(object sender, EventArgs e)
+        {
+            FormChangePass a = new FormChangePass();
+            a.ShowDialog();
+        }
+
+        private void BtnLogout_Click(object sender, EventArgs e)
+        {
+            //if (User == admin)
+            //{
+            //    FormInventory a = new FormInventory();
+            //    this.Close();
+            //    a.Show();
+            //}
+
+            //else
+            //{
+            //    FormLogin a = new FormLogin();
+            //    this.Close();
+            //    a.Show();
+            //}
+        }
+    }
+}
---
+++ KoalaShop/FormProductStocks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace KoalaShop
+{
+    public partial class FormProductStocks : DevExpress.XtraEditors.XtraForm
+    {
+        public FormProductStocks()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
